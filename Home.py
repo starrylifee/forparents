719c72313a32a644112c6473e7fa973fb9bda264
starrylife <forinnocen@naver.com>
2024-07-21 09:00:37 +0900

    githubhidden

--- Home.py
+++ Home.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK{ display: none; }
+    #MainMenu{ visibility: hidden; }
+    footer { visibility: hidden; }
+    header { visibility: hidden; }
+    </style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 st.title("학부모를 위한 인공지능 도구 모음")
 
 col1, col2 = st.columns(2)
--- pages/2 학교에 못갈것 같아요.py
+++ pages/2 학교에 못갈것 같아요.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK{ display: none; }
+    #MainMenu{ visibility: hidden; }
+    footer { visibility: hidden; }
+    header { visibility: hidden; }
+    </style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 st.title("학교에 못갈 것 같아요")
 st.write("""
 학생이 학교에 결석했을 때 관련 안내를 해주는 프로그램입니다.""")
